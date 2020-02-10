74c346b7a8e8711afddd748184679900b7bbefa6
AnnaKostrikova <anna.kostrikova@g.austincc.edu>
2020-02-09 23:18:10 -0600

    C to F convert​

--- main.py
+++ main.py
@@ -1,8 +1,9 @@
 ######################
 # Name: Anna Kostrikova
-# Coding 01
-# shows hello,name
+# Coding 02
+# converting celsius to fahrenheit
 ######################
-name = input("Enter your name: ")
-print("Hello", name)
-
+celsius=18.123
+RATIO=9.0/5.0
+Fahrenheit=RATIO*celsius+32
+print('{:.1f}'.format(celsius),'degrees Celsius is equal to','{:.1f}'.format(Fahrenheit),'degrees Fahrenheit')
